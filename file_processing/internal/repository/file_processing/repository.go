8866bb61e04d4ca99e8494606ca9d4fb580119c8
Valery Cherneykin <chernecin@icloud.com>
2025-07-02 21:19:28 +0300

    Add method for repository List

--- file_processing/internal/repository/file_processing/repository.go
+++ file_processing/internal/repository/file_processing/repository.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/Masterminds/squirrel"
 	"github.com/ValeryCherneykin/taskanalytics/file_processing/internal/client/db"
-	"github.com/ValeryCherneykin/taskanalytics/file_processing/internal/repository"
 	"github.com/ValeryCherneykin/taskanalytics/file_processing/internal/repository/file_processing/model"
 )
 
@@ -26,11 +25,11 @@ type repo struct {
 	db db.Client
 }
 
-func NewRepository(db db.Client) repository.UploadedFileRepository {
-	return &repo{
-		db: db,
-	}
-}
+// func NewRepository(db db.Client) repository.UploadedFileRepository {
+// 	return &repo{
+// 		db: db,
+// 	}
+// }
 
 func (r *repo) Create(ctx context.Context, info model.UploadedFile) (int64, error) {
 	builder := squirrel.Insert(tableName).
@@ -89,4 +88,47 @@ func (r *repo) Get(ctx context.Context, id int64) (*model.UploadedFile, error) {
 	}
 
 	return &file, nil
+}
+
+func (r *repo) List(ctx context.Context, limit, offset int) ([]model.UploadedFile, error) {
+	builder := squirrel.Select(
+		idColumn, fileNameColumn, filePathColumn, sizeColumn,
+		statusColumn, createdAtColumn, updatedAtColumn, deletedAtColumn,
+	).
+		PlaceholderFormat(squirrel.Dollar).
+		From(tableName).
+		OrderBy(createdAtColumn + " DESC").
+		Limit(uint64(limit)).
+		Offset(uint64(offset))
+
+	query, args, err := builder.ToSql()
+	if err != nil {
+		return nil, err
+	}
+
+	q := db.Query{
+		Name:     "file_processing_repository.List",
+		QueryRaw: query,
+	}
+
+	rows, err := r.db.DB().QueryContext(ctx, q, args...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var files []model.UploadedFile
+	for rows.Next() {
+		var file model.UploadedFile
+		if err := rows.Scan(
+			&file.FileID, &file.FileName, &file.FilePath,
+			&file.Size, &file.Status, &file.CreatedAt,
+			&file.UpdatedAt, &file.DeletedAt,
+		); err != nil {
+			return nil, err
+		}
+		files = append(files, file)
+	}
+
+	return files, nil
 }
--- file_processing/internal/repository/repository.go
+++ file_processing/internal/repository/repository.go
@@ -9,4 +9,5 @@ import (
 type UploadedFileRepository interface {
 	Create(ctx context.Context, info model.UploadedFile) (int64, error)
 	Get(ctx context.Context, id string) (model.UploadedFile, error)
+	List(ctx context.Context, limit, offset int) ([]model.UploadedFile, error)
 }
