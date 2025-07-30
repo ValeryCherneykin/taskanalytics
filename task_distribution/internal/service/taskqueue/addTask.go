c20eff7794f06b4080c6e730dc57ecd72cd697e1
Valery Cherneykin <chernecin@icloud.com>
2025-07-30 21:03:45 +0300

    Added kafka in addTask

--- task_distribution/internal/service/taskqueue/addTask.go
+++ task_distribution/internal/service/taskqueue/addTask.go
@@ -2,10 +2,26 @@ package taskqueue
 
 import (
 	"context"
+	"encoding/json"
 
 	"github.com/ValeryCherneykin/taskanalytics/task_distribution/internal/model"
 )
 
 func (s *serv) AddTask(ctx context.Context, task *model.Task) error {
-	return s.queue.Enqueue(ctx, task)
+	err := s.queue.Enqueue(ctx, task)
+	if err != nil {
+		return err
+	}
+
+	data, err := json.Marshal(task)
+	if err != nil {
+		return err
+	}
+
+	err = s.producer.SendMessage(ctx, "file-processing-tasks", []byte(task.TaskID), data)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- task_distribution/internal/service/taskqueue/service.go
+++ task_distribution/internal/service/taskqueue/service.go
@@ -1,16 +1,19 @@
 package taskqueue
 
 import (
+	"github.com/ValeryCherneykin/taskanalytics/task_distribution/internal/client/kafka"
 	"github.com/ValeryCherneykin/taskanalytics/task_distribution/internal/repository"
 	"github.com/ValeryCherneykin/taskanalytics/task_distribution/internal/service"
 )
 
 type serv struct {
-	queue repository.TaskQueue
+	queue    repository.TaskQueue
+	producer kafka.Producer
 }
 
-func NewService(queue repository.TaskQueue) service.QueueService {
+func NewService(queue repository.TaskQueue, producer kafka.Producer) service.QueueService {
 	return &serv{
-		queue: queue,
+		queue:    queue,
+		producer: producer,
 	}
 }
